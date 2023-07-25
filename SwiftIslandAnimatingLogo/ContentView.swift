abf2c51a93fc877b9ab505186335456232b3e121
Paul Peelen <Paul@PaulPeelen.com>
2023-07-25 10:57:41 +0200

    Initial Commit

---
+++ SwiftIslandAnimatingLogo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftIslandAnimatingLogo
+//
+//  Created by Paul Peelen on 2023-07-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftIslandAnimatingLogo/SwiftIslandAnimatingLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIslandAnimatingLogoApp.swift
+//  SwiftIslandAnimatingLogo
+//
+//  Created by Paul Peelen on 2023-07-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIslandAnimatingLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
